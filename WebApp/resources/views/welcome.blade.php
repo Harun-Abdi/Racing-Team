bd600e54d9ec1c75151e7797e05d078a4504dd70
Harun-Abdi <91126094+Harun-Abdi@users.noreply.github.com>
2022-10-09 17:29:50 +0200

    main page

--- WebApp/resources/views/welcome.blade.php
+++ WebApp/resources/views/welcome.blade.php
@@ -2,14 +2,84 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Title of the document</title>
-    <style> </style>
+    <title>Main Dashboard</title>
+    <link rel="stylesheet" href= "styles/Main.css"/>
+
 </head>
 
 
-
-
 <body>
+
+<h1 id="Page-name">Main Dashboard</h1>
+
+    <div class="wow" >
+        <ul>
+            <li>Main</li>
+            <li>Cooling</li>
+            <li>Battery Cell</li>
+            <li>Inverters  </li>
+            <li>Shutdown Circuit </li>
+        </ul>
+
+
+    </div>
+
+
+
+    <div class="grid">
+
+
+        <div id="grid__item">
+            <h1>Speed</h1>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+        </div>
+        <div id="grid__item">
+            <h1>Brake Pressure</h1>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+        </div>
+        <div id="grid__item">
+            <h1>Steering</h1>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+        </div>
+
+
+        <div id="grid__item">
+            <h1>State of charge</h1>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+        </div>
+        <div id="grid__item">
+            <h1>Low voltage battery</h1>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+        </div>
+        <div id="grid__item">
+            <h1>Brake temp</h1>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+            <p>fkdsfjsdfkjsdfjk</p>
+        </div>
+
+
+    </div>
+
+
+
+
 
 </body>
 
